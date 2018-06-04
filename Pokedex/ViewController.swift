3dfe2a0929e8667615703774c380b9a7c7513d45
Tania <taniatehranchi@yahoo.com>
2018-06-04 14:33:14 +0430

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Tania Tehranchi on 6/4/18.
+//  Copyright © 2018 Tania Tehranchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
